191191831d57d32699ce093df3d52f251f11054d
Moshe Braner <moshe.braner@gmail.com>
2024-12-18 20:25:33 -0500

    wrap around menus

--- main/SetupMenu.cpp
+++ main/SetupMenu.cpp
@@ -610,13 +610,13 @@ void SetupMenu::down(int count){
 	ucg->setColor(COLOR_WHITE);
 #if defined(SUNTON28)
 	if( highlight  > -1 ){
-		highlight --;
+		highlight--;
 	}
 	else
 		highlight = (int)(_childs.size() -1 );
 #else
-	while( (highlight  > -1) && count ){
-		highlight --;
+	while( /* (highlight  >= -1) && */ count > 0 ){
+		highlight--;
 		count--;
 	}
 	if( highlight < -1 )
@@ -688,13 +688,13 @@ void SetupMenu::up(int count){
 	ucg->setColor(COLOR_WHITE);
 #if defined(SUNTON28)
 	if( highlight < (int)(_childs.size()-1) ){
-		highlight ++;
+		highlight++;
 	}
 	else
 		highlight = -1;
 #else
-	while( highlight < (int)(_childs.size()-1) && count ){
-		highlight ++;
+	while( /* highlight <= (int)(_childs.size()-1) && */ count > 0 ){
+		highlight++;
 		count--;
 	}
 	if( highlight > (int)(_childs.size()-1) ){
@@ -1819,17 +1819,17 @@ void SetupMenu::options_menu_create_horizon_screen( MenuEntry *top ){
 	plimit->setHelp( "If limited, some 'sky' and some 'ground' will always be visible" );
 	top->addEntry(plimit);
 
+	SetupMenuValFloat * offset = new SetupMenuValFloat( "Horizon Pitch Offset", "°", -10, 10, 0.5, 0, false, &horizon_offset );
+	offset->setPrecision( 1 );
+	offset->setHelp("Move displayed horizon up/down (also via rotary). Returns to zero on reboot. (Not related to autozero)");
+	top->addEntry( offset );
+
 	if (testmode.get()) {
 		SetupMenuSelect * nums = new SetupMenuSelect( "P&B Numbers", RST_NONE, 0, true, &horizon_nums );
 		nums->addEntry( "None");
 		nums->addEntry( "Show");
 		top->addEntry(nums);
 	}
-
-	SetupMenuValFloat * offset = new SetupMenuValFloat( "Horizon Pitch Offset", "°", -10, 10, 0.5, 0, false, &horizon_offset );
-	offset->setPrecision( 1 );
-	offset->setHelp("Move displayed horizon up/down (also via rotary). Returns to zero on reboot. (Not related to autozero)");
-	top->addEntry( offset );
 }
 
 void SetupMenu::options_menu_create_advanced( MenuEntry *top ){
@@ -2443,7 +2443,7 @@ void SetupMenu::system_menu_create_comm_wired( MenuEntry *top ){
 	if( hardwareRevision.get() >= XCVARIO_21 ) {
 		SetupMenu * rs232_2 = new SetupMenu( "RS232 Interface S2" );
 		top->addEntry( rs232_2 );
-		rs232_2->setHelp( "Configure serial interface S2 (reboots)");
+		rs232_2->setHelp( "Configure serial interface S2 (reboots)", 240);
 		rs232_2->addCreator(system_menu_create_interfaceS2);
 	}
 
@@ -2547,7 +2547,7 @@ void SetupMenu::system_menu_create_comm_routing( MenuEntry *top ){
 
 	SetupMenu * w3rt = new SetupMenu( "Port 2000 Routing" );
 	top->addEntry( w3rt );
-	w3rt->setHelp( "Select data sources to be routed from/to WiFi port 2000 (reboots)");
+	w3rt->setHelp( "Select data sources to be routed from/to WiFi port 2000 (reboots)", 220);
 	w3rt->addCreator( system_menu_create_interfaceW3_routing );
 
 	SetupMenuSelectCodes * datamon = new SetupMenuSelectCodes( "Monitor", RST_NONE, data_mon, true, &data_monitor );
