fa0f2bdf8872c71afd51cd1607e6a69598c6b79a
moshe-braner <93451962+moshe-braner@users.noreply.github.com>
2025-02-12 16:09:18 -0500

    <cstdint>

--- main/ApproxMath.h
+++ main/ApproxMath.h
@@ -1,16 +1,30 @@
 /*
  * ApproxMath.h
- * by Moshe Braner, 2022
+ * Copyright (C) 2022 Moshe Braner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef APPROXMATH_H
 #define APPROXMATH_H
 
 float atan2_approx(float, float);
-float atan2_better(float, float);
 float sin_approx(float);
 float cos_approx(float);
-float hypot_approx(float, float);
+float approxHypotenuse(float, float);
+float CosLat(void);
+float InvCosLat(void);
 
 int32_t iatan2_approx( int32_t ns, int32_t ew );
 uint32_t iapproxHypotenuse0( int32_t x, int32_t y );
